7c61fcb892b66403a7ab98013c25ade33e66ec6e
CHern0g0r <fyodorchernogor@gmail.com>
2023-06-05 22:56:15 +0300

    more skeleton

--- lab01/src/model.py
+++ lab01/src/model.py
@@ -1,9 +1,27 @@
 from torch.nn import Module
 
 
+class Coder(Module):
+    def __init__(self,
+                 coder_class,
+                 *args,
+                 weights=None,
+                 **kwargs):
+        super().__init__()
+        self.coder = coder_class(*args)
+        if weights:
+            self.coder.load_state_dict(
+                torch.load(weights)
+            )
+
+    def forward(self, X):
+        out = self.coder(X)
+        return out
+
+
 class Encoder(Module):
     def __init__(self):
-        pass
+        super().__init__()
 
     def forward(self, X):
         return X
@@ -11,6 +29,7 @@ class Encoder(Module):
 
 class Decoder(Module):
     def __init__(self):
+        super().__init__()
         pass
 
     def forward(self, X):
@@ -18,7 +37,13 @@ class Decoder(Module):
 
 
 class Pipeline(Module):
+
+    @staticmethod
+    def create(classes):
+        pass
+
     def __init__(self, encoder, decoder, quant):
+        super().__init__()
         self.enc = encoder
         self.dec = decoder
         self.q = quant
---
+++ lab01/src/test.py
@@ -0,0 +1,5 @@
+from src.model import Coder, Encoder
+
+
+if __name__ == '__main__':
+    Coder(Encoder)
